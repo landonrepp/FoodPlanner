c7ec8e34a3df7757ea64a03bcc338a562575e702
landonrepp <landonrepp@gmail.com>
2019-07-06 13:00:43 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/app/app.component.html
    	modified:   src/app/app.module.ts
    	modified:   src/styles.css
    
    Untracked files:
    	src/app/components/common/sidebar/

--- foodPlanner/src/app/app.module.ts
+++ foodPlanner/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/common/header/header.component';
+import { SidebarComponent } from './components/common/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
